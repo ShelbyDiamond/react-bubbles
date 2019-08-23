d5515f76428bf6fcef61434653240708dd2670cc
Shelby Diamond <Shelbydiamond518@gmail.com>
2019-08-23 09:54:10 -0700

    updated axios with auth, private router
    and bubblepage rendering

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,55 @@
-import React from "react";
+import React, { useState } from "react"
+import axios from "axios"
 
-const Login = () => {
+const Login = props => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  // credentials are username and password used to login
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: ""
+  })
+
+  const handleChanger = event => {
+    setCredentials({
+      ...credentials,
+      [event.target.name]: event.target.value
+    })
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    axios
+      .post("http://localhost:5000/api/login", credentials)
+      .then(
+        response => (
+          localStorage.setItem("token", response.data.payload),
+          props.history.push("/protected")
+        )
+      )
+      .catch(error => console.log("Failure", error))
+    console.log(credentials)
+  }
   return (
     <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          name="username"
+          value={credentials.username}
+          onChange={handleChanger}
+          required
+        />
+        <input
+          name="password"
+          value={credentials.password}
+          onChange={handleChanger}
+          type="password"
+          required
+        />
+        <button>Submit</button>
+      </form>
     </>
-  );
-};
+  )
+}
 
-export default Login;
+export default Login

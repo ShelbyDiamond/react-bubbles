f6fd64f1985636f5ad07e500ee98abc1cc8e1d2b
Shelby Diamond <Shelbydiamond518@gmail.com>
2019-08-23 10:12:35 -0700

    has some bugs that weren't there a minute ago.
    Saving code before I debug

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,31 +1,36 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React, { useState } from "react"
+import axios from "axios"
+import { axiosWithAuth } from "../utilities/axiosWithAuth"
 
 const initialColor = {
   color: "",
   code: { hex: "" }
-};
+}
 
 const ColorList = ({ colors, updateColors }) => {
-  console.log(colors);
-  const [editing, setEditing] = useState(false);
-  const [colorToEdit, setColorToEdit] = useState(initialColor);
+  console.log(colors)
+  const [editing, setEditing] = useState(false)
+  const [colorToEdit, setColorToEdit] = useState(initialColor)
 
   const editColor = color => {
-    setEditing(true);
-    setColorToEdit(color);
-  };
+    setEditing(true)
+    setColorToEdit(color)
+  }
 
   const saveEdit = e => {
-    e.preventDefault();
+    e.preventDefault()
+    axiosWithAuth()
+      .put(`http://localhost:5000/api/colors/${colorToEdit.id}`)
+      .then(response => console.log(response))
+      .catch(err => console.log(err))
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
-  };
+  }
 
   const deleteColor = color => {
     // make a delete request to delete this color
-  };
+  }
 
   return (
     <div className="colors-wrap">
@@ -79,7 +84,7 @@ const ColorList = ({ colors, updateColors }) => {
       <div className="spacer" />
       {/* stretch - build another form here to add a color */}
     </div>
-  );
-};
+  )
+}
 
-export default ColorList;
+export default ColorList
